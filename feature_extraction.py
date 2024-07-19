b8d051c682a87c9bb1534149eb0ff8cf91d2300d
HosseinEn <hosseinenshaei@stu.nit.ac.ir>
2024-07-19 17:31:24 +0330

    add PCA for feature dimension reduction

--- feature_extraction.py
+++ feature_extraction.py
@@ -5,6 +5,7 @@ from sklearn.neighbors import LocalOutlierFactor
 from scipy.stats import skew, kurtosis
 from scipy.signal import welch
 import antropy as ant
+from sklearn.decomposition import PCA
 
 def wavelet_transform(signal, wavelet='db4', level=4):
     coeffs = pywt.wavedec(signal, wavelet, level=level)
@@ -17,21 +18,17 @@ def power_spectral_density(signal, fs=256):
 def extract_features(eeg_signals):
     features = []
     for signal in eeg_signals:
-        # Wavelet transform
         wavelet_features = wavelet_transform(signal)
 
-        # ICA
         ica = FastICA(n_components=1, random_state=10)
         ica_features = ica.fit_transform(signal.reshape(-1, 1)).flatten()
 
-        # Statistical features
         var = np.var(signal)
         skewness = skew(signal)
         kurt = kurtosis(signal)
         rms = np.sqrt(np.mean(signal**2))
         entropy = ant.spectral_entropy(signal, sf=256, method='welch')
 
-        # Power spectral density
         psd = power_spectral_density(signal)
 
         # LOF
@@ -39,8 +36,14 @@ def extract_features(eeg_signals):
         lof_score = lof.fit_predict(signal.reshape(-1, 1))
 
         # Combine features
-        # feature_vector = np.concatenate([ica_features, [var, skewness, kurt, rms, entropy], psd])
-        feature_vector = np.concatenate([ica_features, [], psd])
+        feature_vector = np.concatenate([ica_features, [var, skewness, kurt, rms, entropy], psd]) # Acc: 92.57, F1: 92.59, Prec: 92.94
+        # feature_vector = np.concatenate([ica_features, [var, rms, entropy], psd]) # Acc: 92.59, F1: 92.60, Prec: 92.59
+        # feature_vector = np.concatenate([ica_features, [rms, entropy], psd]) # Acc: 92.61, F1: 92.63, Prec: 93.09
+        # feature_vector = np.concatenate([ica_features, [], psd]) # Acc: 91.15, F1: 91.14, Prec: 91.35
+
         features.append(feature_vector)
 
-    return np.array(features)
+    pca = PCA(n_components=0.95)
+    pca_features = pca.fit_transform(features)
+
+    return np.array(pca_features)
