4e0cc38291357aa7d1211382cbfc4daf9c5549ce
HosseinEn <hosseinenshaei@stu.nit.ac.ir>
2024-07-20 22:35:47 +0330

    update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
-# run the py main.py for 10 times
-# arange between -7 to 4 with step 0.5
-SNR_VALUES=$(seq -7 0.5 -3)
+LOWER_SNR=-2
+UPPER_SNR=8
+STEP=0.5
+SNR_VALUES=$(seq $LOWER_SNR $STEP $UPPER_SNR)
 for SNR_VALUE in $SNR_VALUES
 do
-  # write the SNR value to the config.yaml file under data > snr_db
-  sed -i "s/snr_db: .*$/snr_db: $SNR_VALUE/" config.yaml
-  for i in {1..10}
-  do
-      python3 main.py > /dev/null 2>&1
-  done
+    python3 main.py --snr_db "$SNR_VALUE" --mode train > /dev/null 2>&1
+    for i in {1..3}
+    do
+        python3 main.py --snr_db "$SNR_VALUE" --mode test > /dev/null 2>&1
+    done
 done
 
+#py -c "import pandas as pd; df = pd.read_csv('result.csv'); print(df.describe())"
