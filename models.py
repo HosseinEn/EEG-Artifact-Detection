6a44107aa00558501725deb3914684f1c3103fd2
HosseinEn <hosseinenshaei@stu.nit.ac.ir>
2024-08-12 10:01:12 +0330

    models.py: improvements

--- models.py
+++ models.py
@@ -1,3 +1,4 @@
+import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -25,19 +26,19 @@ class ExtremeLearningMachine(nn.Module):
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.output_dim = output_dim
-        self.input_weights = nn.Parameter(torch.randn(self.input_dim, self.hidden_dim), requires_grad=False)
+        self.w_in = nn.Parameter(torch.randn(self.input_dim, self.hidden_dim), requires_grad=False)
         self.bias = nn.Parameter(torch.randn(self.hidden_dim), requires_grad=False)
-        self.output_weights = nn.Parameter(torch.randn(hidden_dim, output_dim))
+        self.w_out = nn.Parameter(torch.randn(hidden_dim, output_dim))
 
     def forward(self, x):
-        h = F.sigmoid(torch.mm(x, self.input_weights) + self.bias)
-        y = torch.mm(h, self.output_weights)
+        h = F.sigmoid(torch.mm(x, self.w_in) + self.bias)
+        y = torch.mm(h, self.w_out)
         return y
 
     def train_elm(self, x_train, y_train):
         y_train = F.one_hot(y_train.long(), num_classes=self.output_dim)
-        H = F.sigmoid(torch.matmul(x_train, self.input_weights) + self.bias)
-        H_pinv = torch.pinverse(H)
-        output_weights_new = torch.matmul(H_pinv, y_train.float())
+        H = F.sigmoid(torch.matmul(x_train, self.w_in) + self.bias)
+        H_pinv = torch.pinverse(H) # (H^T H)^-1 H^T
+        w_out_new = torch.matmul(H_pinv, y_train.float())
         with torch.no_grad():
-            self.output_weights.copy_(output_weights_new)
+            self.w_out.copy_(w_out_new)
