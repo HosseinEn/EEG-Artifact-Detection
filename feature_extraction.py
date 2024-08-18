41499f8847056d6866cf42483396085dfee59758
HosseinEn <hosseinenshaei@stu.nit.ac.ir>
2024-08-18 11:21:13 +0330

    feature_extraction.py: remove the wavelet details

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,14 +1,12 @@
 import numpy as np
 import pywt
-from scipy.stats import skew, kurtosis
 from scipy.signal import welch
-import antropy as ant
 
 def wavelet_transform(signal, wavelet='db4', level=4):
     coeffs = pywt.wavedec(signal, wavelet, level=level)
     approx = coeffs[0]
     details = coeffs[1:]
-    return np.concatenate([approx.flatten()] + [d.flatten() for d in details[1:]])
+    return np.concatenate([approx.flatten()])
 
 def power_spectral_density(signal, fs=512):
     freqs, psd = welch(signal, fs=fs)
@@ -17,8 +15,8 @@ def power_spectral_density(signal, fs=512):
 def extract_features(eeg_signals):
     features = []
     for signal in eeg_signals:
-        wavelet_features = wavelet_transform(signal)
-        psd = power_spectral_density(signal)
-        feature_vector = np.concatenate([wavelet_features, psd])
+        # wavelet_features = wavelet_transform(signal)
+        # psd = power_spectral_density(signal)
+        feature_vector = np.concatenate([signal])
         features.append(feature_vector)
     return np.array(features)
