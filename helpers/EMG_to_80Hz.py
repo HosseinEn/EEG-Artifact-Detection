cc66135c0a131e5410c1de3a8dc2791419448822
HosseinEn <hosseinenshaei@stu.nit.ac.ir>
2024-08-30 16:54:02 +0330

    EMG_to_80Hz.py: filter EMG from EEGdenoiseNet between 1 and 80 Hz

---
+++ helpers/EMG_to_80Hz.py
@@ -0,0 +1,60 @@
+import numpy as np
+import scipy.io as sio
+import scipy.signal as signal
+import matplotlib.pyplot as plt
+from scipy.signal import welch, butter, filtfilt, firwin, lfilter
+from scipy.io import savemat
+
+def butterworth_filter(data, cutoff, fs, order=10):
+    nyquist = 0.5 * fs
+    normal_cutoff = cutoff / nyquist
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    filtered_data = filtfilt(b, a, data)
+    return filtered_data
+
+def fir_filter(data, cutoff, fs, numtaps=401):
+    nyquist = 0.5 * fs
+    normal_cutoff = cutoff / nyquist
+    fir_coeff = firwin(numtaps, normal_cutoff)
+    filtered_data = lfilter(fir_coeff, 1.0, data)
+    return filtered_data
+
+def power_spectral_density(signal, fs=256):
+    freqs, psd = welch(signal, fs=fs)
+    return freqs, psd
+
+
+if __name__ == '__main__':
+    # Assuming you're loading from a .mat file, replace with actual path to your data
+    emg_signals = sio.loadmat('data/EMG_all_epochs.mat')['EMG_all_epochs']
+
+    # Filter parameters
+    cutoff_frequency = 80  # Hz
+    sampling_frequency = 256  # Hz
+    filtered_emg_signals = np.zeros_like(emg_signals)
+
+    # Apply the Butterworth filter to each epoch
+    for i in range(emg_signals.shape[0]):
+        filtered_emg_signals[i, :] = fir_filter(emg_signals[i, :], cutoff_frequency, sampling_frequency)
+
+    # Select a random epoch to visualize
+    idx = 10
+
+    # Calculate the PSD before and after filtering
+    freqs, psd_before = power_spectral_density(emg_signals[idx, :], fs=sampling_frequency)
+    freqs, psd_after = power_spectral_density(filtered_emg_signals[idx, :], fs=sampling_frequency)
+
+    # Plot the PSD
+    # plt.figure(figsize=(10, 6))
+    # plt.semilogy(freqs, psd_before, label='Before Filtering')
+    # plt.semilogy(freqs, psd_after, label='After Filtering')
+    # plt.xlabel('Frequency (Hz)')
+    # plt.ylabel('Power Spectral Density (V^2/Hz)')
+    # plt.title('Power Spectral Density of EMG Signal')
+    # plt.legend()
+    # plt.show()
+
+    # np.save('data/filtered_EMG_all_epochs.npy', filtered_emg_signals)
+    savemat('data/filtered80Hz_EMG_all_epochs.mat', {'EMG_all_epochs': filtered_emg_signals})
+
+
