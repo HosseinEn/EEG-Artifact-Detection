c46fa12d15f7e3f36b6e25e695942a519af18357
HosseinEn <hosseinenshaei@stu.nit.ac.ir>
2024-09-01 12:53:53 +0330

    combined_data_plotter.py, psd_plotter.py: improve plotting

--- helpers/combined_data_plotter.py
+++ helpers/combined_data_plotter.py
@@ -4,7 +4,7 @@ from sklearn.decomposition import FastICA
 from scipy.signal import welch
 from sklearn.preprocessing import StandardScaler
 
-def power_spectral_density(signal, fs=512):
+def power_spectral_density(signal, fs=256):
     freqs, psd = welch(signal, fs=fs)
     return freqs, psd
 
@@ -13,30 +13,56 @@ def data_with_noise(data, labels, noise_no, n):
     return data[index], index
 
 n_comp = 80
-index = 100
+index = np.random.random_integers(1,700)
 
 random_state = 42
 
-for SNR in [-6.0, 6.0]:
-    data_snr = np.load(f'data/test/snr {str(SNR)}/X.npy')
-    data_snr_label = np.load(f'data/test/snr {str(SNR)}/Y.npy')
+data = np.load('data/test/snr -7.0/X.npy')
+data_label = np.load('data/test/snr -7.0/Y.npy')
 
-    ica = FastICA(n_components=n_comp, random_state=random_state)
-    res = ica.fit_transform(data_snr)
+clean = data_with_noise(data, data_label, 0, index)
+eog = data_with_noise(data, data_label, 1, index)
+emg = data_with_noise(data, data_label, 2, index)
 
-    data_clean, real_clean_idx = data_with_noise(data_snr, data_snr_label, 0, index)
-    data_eog, real_eog_idx = data_with_noise(data_snr, data_snr_label, 1, index)
-    data_emg, real_emg_idx = data_with_noise(data_snr, data_snr_label, 2, index)
-
-    plt.subplot(2, 1, 1)
-    plt.title('Signal - EMG Data')
-    plt.plot(data_emg, label='Original - SNR: ' + str(SNR))
-    plt.legend()
-
-    plt.subplot(2, 1, 2)
-    plt.title('ICA')
-    plt.plot(res[real_emg_idx], label=f'n_components={n_comp} - SNR: {str(SNR)}')
-    plt.legend()
-
+# calculate the power spectral density
+freqs, psd = power_spectral_density(clean[0])
+# plt.subplot(2, 1, 1)
+plt.title('Log PSD for three signals contaminated with noise, SNR=-7.0', fontsize=14)
+plt.semilogy(freqs, psd, label='Clean')
 plt.legend()
+
+freqs, psd = power_spectral_density(eog[0])
+# plt.subplot(3, 1, 2)
+plt.xticks(range(0,256,10))
+plt.semilogy(freqs, psd, label='EEG+EOG')
+plt.legend()
+
+freqs, psd = power_spectral_density(emg[0])
+# plt.subplot(2, 1, 2)
+plt.semilogy(freqs, psd, label='EEG+EMG')
+plt.xlabel('Frequency (Hz)', fontsize=14, fontweight='bold')
+plt.ylabel('Log PSD (V^2/Hz)', fontsize=14, fontweight='bold')
+
+# for SNR in [-6.0, 6.0]:
+#     data_snr = np.load(f'data/test/snr {str(SNR)}/X.npy')
+#     data_snr_label = np.load(f'data/test/snr {str(SNR)}/Y.npy')
+#
+#     ica = FastICA(n_components=n_comp, random_state=random_state)
+#     res = ica.fit_transform(data_snr)
+#
+#     data_clean, real_clean_idx = data_with_noise(data_snr, data_snr_label, 0, index)
+#     data_eog, real_eog_idx = data_with_noise(data_snr, data_snr_label, 1, index)
+#     data_emg, real_emg_idx = data_with_noise(data_snr, data_snr_label, 2, index)
+#
+#     plt.subplot(2, 1, 1)
+#     plt.title('Signal - EMG Data')
+#     plt.plot(data_emg, label='Original - SNR: ' + str(SNR))
+#     plt.legend()
+#
+#     plt.subplot(2, 1, 2)
+#     plt.title('ICA')
+#     plt.plot(res[real_emg_idx], label=f'n_components={n_comp} - SNR: {str(SNR)}')
+#     plt.legend()
+
+plt.legend(fontsize=14)
 plt.show()
--- helpers/psd_plotter.py
+++ helpers/psd_plotter.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.signal import welch
+from scipy.signal import welch, butter
 import scipy.io as sio
 
 # /home/hossein/University4022/Project/EEG/workspace/SCEADNN/data/EEG_all_epochs.mat
@@ -15,22 +15,24 @@ def power_spectral_density(signal, fs=256):
 # show the power of one sample using psd welch
 
 if __name__ == '__main__':
+    idx = 100
     # load the data
-    eeg_signals = sio.loadmat('data/EEG_all_epochs.mat')['EEG_all_epochs']
+    eeg_signals = sio.loadmat('data/filtered120Hz_EEG_all_epochs')['EEG_all_epochs']
     eog_signals = sio.loadmat('data/EOG_all_epochs.mat')['EOG_all_epochs']
-    emg_signals = sio.loadmat('data/EMG_all_epochs.mat')['EMG_all_epochs']
+    emg_signals = sio.loadmat('data/filtered120Hz_EMG_all_epochs.mat')['EMG_all_epochs']
 
     # show the power of each using psd welch
-    freqs, psd = power_spectral_density(eeg_signals[0])
+    freqs, psd = power_spectral_density(eeg_signals[idx])
     plt.semilogy(freqs, psd, label='EEG')
 
-    freqs, psd = power_spectral_density(eog_signals[0])
+    freqs, psd = power_spectral_density(eog_signals[idx])
     plt.semilogy(freqs, psd, label='EOG')
 
-    freqs, psd = power_spectral_density(emg_signals[0])
+    freqs, psd = power_spectral_density(emg_signals[idx])
     plt.semilogy(freqs, psd, label='EMG')
 
-    plt.xlabel('Frequency (Hz)')
-    plt.ylabel('Log PSD')
-    plt.legend()
+    plt.title('Random Samples of Signals in EEGDenoiseNet dataset', fontsize=14)
+    plt.xlabel('Frequency (Hz)', fontsize=14, fontweight='bold')
+    plt.ylabel('Log PSD (V^2/Hz)', fontsize=14, fontweight='bold')
+    plt.legend(fontsize=14)
     plt.show()
