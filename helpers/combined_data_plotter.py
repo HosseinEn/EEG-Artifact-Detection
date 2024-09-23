5cf529cd08d953f296a599712668e561c2208984
HosseinEn <hosseinenshaei@stu.nit.ac.ir>
2024-09-23 18:42:00 +0330

    combined_data_plotter.py, psd_plotter.py: add global fontsize

--- helpers/combined_data_plotter.py
+++ helpers/combined_data_plotter.py
@@ -4,6 +4,9 @@ from sklearn.decomposition import FastICA
 from scipy.signal import welch
 from sklearn.preprocessing import StandardScaler
 
+plt.rcParams.update({'font.size': 14})
+plt.figure(figsize=(8, 6))
+
 def power_spectral_density(signal, fs=256):
     freqs, psd = welch(signal, fs=fs)
     return freqs, psd
@@ -40,8 +43,8 @@ plt.legend()
 freqs, psd = power_spectral_density(emg[0])
 # plt.subplot(2, 1, 2)
 plt.semilogy(freqs, psd, label='EEG+EMG')
-plt.xlabel('Frequency (Hz)', fontsize=14, fontweight='bold')
-plt.ylabel('Log PSD (V^2/Hz)', fontsize=14, fontweight='bold')
+plt.xlabel('Frequency (Hz)', fontweight='bold')
+plt.ylabel('Log PSD (V^2/Hz)', fontweight='bold')
 
 # for SNR in [-6.0, 6.0]:
 #     data_snr = np.load(f'data/test/snr {str(SNR)}/X.npy')
@@ -65,5 +68,5 @@ plt.ylabel('Log PSD (V^2/Hz)', fontsize=14, fontweight='bold')
 #     plt.legend()
 
 plt.xlim([1,85])
-plt.legend(fontsize=14)
+plt.legend()
 plt.show()
--- helpers/psd_plotter.py
+++ helpers/psd_plotter.py
@@ -3,25 +3,22 @@ import matplotlib.pyplot as plt
 from scipy.signal import welch, butter
 import scipy.io as sio
 
-# /home/hossein/University4022/Project/EEG/workspace/SCEADNN/data/EEG_all_epochs.mat
-# /home/hossein/University4022/Project/EEG/workspace/SCEADNN/data/EOG_all_epochs.mat
-# /home/hossein/University4022/Project/EEG/workspace/SCEADNN/data/EMG_all_epochs.mat
+plt.rcParams.update({'font.size': 14})
 
-# show the power of each using psd welch
 def power_spectral_density(signal, fs=256):
     freqs, psd = welch(signal, fs=fs)
     return freqs, psd
 
-# show the power of one sample using psd welch
+
 
 if __name__ == '__main__':
+    plt.figure(figsize=(8, 6))
     idx = 1000
     # load the data
     eeg_signals = sio.loadmat('data/EEG_all_epochs')['EEG_all_epochs']
     eog_signals = sio.loadmat('data/EOG_all_epochs.mat')['EOG_all_epochs']
     emg_signals = sio.loadmat('data/filtered80Hz_EMG_all_epochs.mat')['EMG_all_epochs']
 
-    # show the power of each using psd welch
     freqs, psd = power_spectral_density(eeg_signals[idx])
     plt.semilogy(freqs, psd, label='EEG')
 
@@ -32,8 +29,8 @@ if __name__ == '__main__':
     plt.semilogy(freqs, psd, label='EMG')
 
     # plt.title('Random Samples of Signals in EEGDenoiseNet dataset', fontsize=14)
-    plt.xlabel('Frequency (Hz)', fontsize=14, fontweight='bold')
-    plt.ylabel('Log PSD (V^2/Hz)', fontsize=14, fontweight='bold')
+    plt.xlabel('Frequency (Hz)',fontweight='bold')
+    plt.ylabel('Log PSD (V^2/Hz)',fontweight='bold')
     plt.xlim([1,85])
-    plt.legend(fontsize=14)
+    plt.legend()
     plt.show()
