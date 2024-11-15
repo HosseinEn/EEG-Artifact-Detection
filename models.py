0074fafe8ebb8034cf0a3687a3cff7afc4ab82df
HosseinEn <hosseinenshaei@stu.nit.ac.ir>
2024-11-15 19:12:58 +0330

    models.py: remove batch norms

--- models.py
+++ models.py
@@ -13,9 +13,7 @@ class ArtifactDetectionNN(nn.Module):
     def __init__(self, input_dim):
         super(ArtifactDetectionNN, self).__init__()
         self.fc1 = nn.Linear(input_dim, 128)
-        self.bn1 = nn.BatchNorm1d(128)
         self.fc2 = nn.Linear(128, 64)
-        self.bn2 = nn.BatchNorm1d(64)
         self.fc3 = nn.Linear(64, 4)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(0.6)
@@ -24,9 +22,9 @@ class ArtifactDetectionNN(nn.Module):
         torch.nn.init.kaiming_normal_(self.fc3.weight, nonlinearity='relu')
 
     def forward(self, x):
-        x = self.relu(self.bn1(self.fc1(x)))
+        x = self.relu(self.fc1(x))
         x = self.dropout(x)
-        x = self.relu(self.bn2(self.fc2(x)))
+        x = self.relu(self.fc2(x))
         x = self.fc3(x)
         return x
 
